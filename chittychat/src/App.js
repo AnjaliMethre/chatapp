e3a1cbad25239569081a6e5edcbf5cd30e23c6bc
Anjali Methre <sonaanjali2211@gmail.com>
2025-02-12 19:30:19 +0530

    imported bootstrap, font-awesome and socket.io-client

---
+++ chittychat/src/App.js
@@ -0,0 +1,30 @@
+//bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+//font-awesome
+import "font-awesome/css/font-awesome.css";
+//socket.io-client
+import { io } from "socket.io-client";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
