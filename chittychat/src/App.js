36c411aa2f90893485287beb8ccb347eaf1f24ac
Anjali Methre <sonaanjali2211@gmail.com>
2025-02-13 19:17:38 +0530

    added components

--- chittychat/src/App.js
+++ chittychat/src/App.js
@@ -3,27 +3,38 @@ import "bootstrap/dist/css/bootstrap.min.css";
 //font-awesome
 import "font-awesome/css/font-awesome.css";
 //socket.io-client
-import { io } from "socket.io-client";
-import React from "react";
+//import { io } from "socket.io-client";
+import React, { useState } from "react";
+import NewUser from "./components/NewUser";
 
 function App() {
+  //username
+  const [newUser, setNewUser]=useState("");
+  const[user,setUser]=useState("");
+
+  function handleChange({ currentTarget: input }) {
+    setNewUser(input.value);
+
+  }
+
+  function logNewUser(){
+    setUser(newUser);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="content">
+      <div className="container mt-3">
+        {user && <div>Logged in as {user}</div>}
+        {!user &&
+          <NewUser 
+            newUser={newUser} 
+            handleChange={handleChange} 
+            logNewUser={logNewUser} 
+          />
+        }
+        
+      </div>
+    </main>
   );
 }
 
