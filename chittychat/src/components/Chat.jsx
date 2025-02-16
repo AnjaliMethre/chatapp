080a3a6d0b96d7b5d7793d510189f08a751c1a4e
Anjali Methre <sonaanjali2211@gmail.com>
2025-02-16 22:38:34 +0530

    created chat container

--- chittychat/src/components/Chat.jsx
+++ chittychat/src/components/Chat.jsx
@@ -1,20 +1,15 @@
 import React from 'react'
 import chatHeader from './chatHeader';
 import ChatInput from './ChatInput';
+import ChatContainer from './ChatContainer';
+
 
 const Chat = ({ user, message, setMessage }) => {
   return (
-    <div className="card border-2 border-info w-100">
-            <div className="row vh-95">
-              <div className="d-flex flex-column col-12 col-lg-12 col-xl-12">
-               {/*chat Header*/}
-               <chatHeader user={user} />
-               {/*chat Header*/}
-                
-              </div>
-
-            </div>
-          </div>
+          <ChatContainer>
+            <chatHeader user={user} />
+            <ChatInput message={message} setMessage={setMessage} />
+          </ChatContainer>
   )
 }
 
